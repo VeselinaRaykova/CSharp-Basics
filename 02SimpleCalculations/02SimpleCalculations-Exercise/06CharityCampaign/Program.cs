7f6fc9c387986704a0822130957829e173095b5f
VeselinaRaykova <veselinaraikova@gmail.com>
2019-06-11 11:31:39 +0300

    Add Simple Calculations Exercise

--- 02SimpleCalculations/02SimpleCalculations-Exercise/06CharityCampaign/Program.cs
+++ 02SimpleCalculations/02SimpleCalculations-Exercise/06CharityCampaign/Program.cs
@@ -10,6 +10,22 @@ namespace _06CharityCampaign
     {
         static void Main(string[] args)
         {
+            int days = int.Parse(Console.ReadLine());
+            int bakers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakePrice = 45;
+            double wafflePrice = 5.80;
+            double pancakePrice = 3.20;
+
+            double bakedPerDay = cakes * cakePrice + waffles * wafflePrice + pancakes * pancakePrice;
+            double collectedMoney = days * bakers * bakedPerDay;
+            double costs = 1 / 8f * collectedMoney;
+            collectedMoney -= costs;
+
+            Console.WriteLine($"{collectedMoney:f2}");
 
         }
     }
---
+++ 02SimpleCalculations/02SimpleCalculations-Exercise/07AlcoholMarket/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07AlcoholMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wiskeyPrice = double.Parse(Console.ReadLine()); //lv
+            double beer = double.Parse(Console.ReadLine());
+            double wine = double.Parse(Console.ReadLine());
+            double rakia = double.Parse(Console.ReadLine());
+            double wiskey = double.Parse(Console.ReadLine());
+
+            double rakiaPrice = wiskeyPrice / 2;
+            double winePrice = rakiaPrice * 0.60; //rakiaPrice - rakiaPrice * 0.40
+            double beerPrice = rakiaPrice * 0.20;
+            double money = beer * beerPrice + wine * winePrice + rakia * rakiaPrice + wiskey * wiskeyPrice;
+
+            Console.WriteLine($"{money:f2}");
+        }
+    }
+}
