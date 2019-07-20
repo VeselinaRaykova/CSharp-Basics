ad0076130a29223f3e7feff8d044688e1c392a45
VeselinaRaykova <veselinaraikova@gmail.com>
2019-07-20 23:12:11 +0300

    [basics] Add Nested Loops Labs, part2

--- 07NestedLoops/01NestedLoops-Labs/08CookieFactory/Program.cs
+++ 07NestedLoops/01NestedLoops-Labs/08CookieFactory/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08CookieFactory
 {
@@ -10,6 +6,39 @@ namespace _08CookieFactory
     {
         static void Main(string[] args)
         {
+            int parts = int.Parse(Console.ReadLine());
+            int flour = 0;
+            int eggs = 0;
+            int sugar = 0;
+
+            for (int i = 1; i <= parts; i++)
+            {
+                string product = Console.ReadLine();
+                while (product != "Bake!")
+                {
+                    switch (product)
+                    {
+                        case "flour": flour++; break;
+                        case "eggs": eggs++; break;
+                        case "sugar": sugar++; break;
+                        default:
+                            break;
+                    }
+
+                    product = Console.ReadLine();
+                }
+
+                if (flour < 1 || eggs < 1 || sugar < 1)
+                {
+                    Console.WriteLine($"The batter should contain flour, eggs and sugar!");
+                    i--;
+                }
+                else
+                {
+                    Console.WriteLine($"Baking batch number {i}...");
+                    flour = eggs = sugar = 0;
+                }
+            }
         }
     }
 }
---
+++ 07NestedLoops/01NestedLoops-Labs/09MagicNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09MagicNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int firstDigit = 1; firstDigit <= 9; firstDigit++)
+            {
+                for (int secondDigit = 1; secondDigit <= 9; secondDigit++)
+                {
+                    for (int thirdDigit = 1; thirdDigit <= 9; thirdDigit++)
+                    {
+                        for (int fourthDigit = 1; fourthDigit <= 9; fourthDigit++)
+                        {
+                            for (int fifthDigit = 1; fifthDigit <= 9; fifthDigit++)
+                            {
+                                for (int sixthDigit = 1; sixthDigit <= 9; sixthDigit++)
+                                {
+                                    int result = firstDigit * secondDigit * thirdDigit * fourthDigit * fifthDigit * sixthDigit;
+                                    if (result == magicNumber)
+                                    {
+                                        Console.Write($"{firstDigit}{secondDigit}{thirdDigit}{fourthDigit}{fifthDigit}{sixthDigit} ");
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
