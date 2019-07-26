2f2029e9452a768003aa818eb85f9fc0d9e3ef09
VeselinaRaykova <veselinaraikova@gmail.com>
2019-07-26 20:39:22 +0300

    Add solutions for July, 2019 Exam

--- 10FormerExams/01Exam-6-and-7-July-2019-group1/06NameGame/Program.cs
+++ 10FormerExams/01Exam-6-and-7-July-2019-group1/06NameGame/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06NameGame
 {
@@ -31,7 +27,7 @@ namespace _06NameGame
                     }
                 }
 
-                if (points > maxPoints)
+                if (points >= maxPoints)
                 {
                     maxPoints = points;
                     winner = name;
